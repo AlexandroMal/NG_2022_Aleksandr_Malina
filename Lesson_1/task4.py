5bc14efa6d88e7dba4b24fb41820efe3f532b6ba
Alexandr Malina <Sashamalina8695@gmail.com>
2022-10-28 19:01:04 +0300

    1) Moved (print(discriminant = %.3f % discriminant)). 2) Changed the output code of answers

--- Lesson_1/task4.py
+++ Lesson_1/task4.py
@@ -1,28 +1,25 @@
 print ("======================================")
-print("Quadratic equation: ax^2 + bx + c = 0:")
+print("Quadratic equation: ax^2 + bx + c = 0")
 print ("======================================")
 number_a = float(input("Enter the number a: "))
 number_b = float(input("Enter the number b: "))
 number_c = float(input("Enter the number c: "))
-print ("==================================")
+print ("======================================")
    
 discriminant = ((number_b*number_b) - 4 * number_a * number_c)
+print ("Discriminant= %.3f" % discriminant)
 
 if discriminant > 0:
-    print ("Discriminant= %.3f" % discriminant)
     print ("Roots of the equation:")
     x_1 = (- number_b + (discriminant ** 0.5)) / (2 * number_a)
-    print("x1 = %.3f" % x_1)
     x_2 = (- number_b - (discriminant ** 0.5)) / (2 * number_a)
-    print("x2 = %.3f" % x_2)
+    print ("x1 = %.3f" % x_1,"x2 = %.3f" % x_2, sep="\n" )
 
 if discriminant == 0:
-    print ("Discriminant= 0")
     x = (- number_b / (2 * number_a))
     print("x = %.3f" % x)
 
 if discriminant < 0:
-    print ("Discriminant = %.3f" % discriminant)
     print ("There are no roots of the equation")
 
    

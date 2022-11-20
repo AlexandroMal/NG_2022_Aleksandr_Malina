98859888c69ddca8e7b742d1538ad5a2ae6eac29
Alexandr Malina <Sashamalina8695@gmail.com>
2022-11-20 13:51:01 +0200

    Сhanged variable names

--- Lesson_2/task3.py
+++ Lesson_2/task3.py
@@ -1,6 +1,6 @@
 inputNumber = int(input("Input size: "))
 
-for a in range(inputNumber, 0, -1):
-    for b in range(a, 0, -1):
-        print(b, end = " ")
+for totalNumber in range(inputNumber, 0, -1):
+    for numbers in range(totalNumber, 0, -1):
+        print(numbers, end = " ")
     print("\r")

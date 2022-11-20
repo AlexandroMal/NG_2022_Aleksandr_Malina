1b9b1515c54d51e0e693ddbc6eb83a298fb06cf3
Alexandr Malina <Sashamalina8695@gmail.com>
2022-11-20 13:53:23 +0200

    Сhanged variable names

--- Lesson_2/task4.py
+++ Lesson_2/task4.py
@@ -1,13 +1,13 @@
 InputNumber = int(input("Enter the factorial number: "))
-variable = 1
+multiplier = 1
 
 if InputNumber == 0:
    print ("The factorial of the number is 0")
 else:   
   while InputNumber > 0:
-   factorial = InputNumber * variable
+   factorial = InputNumber * multiplier
    InputNumber = InputNumber - 1
-   variable = factorial
+   multiplier = factorial
 
   print ("Factorial of the number: ", factorial)
 

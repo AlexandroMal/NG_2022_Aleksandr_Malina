c3be5924d0d4baed6e30256ca6eaaa498b6dde30
Alexandr Malina <Sashamalina8695@gmail.com>
2023-01-12 21:25:00 +0200

    moved the output with the result of the operation

--- Lesson_3/task1.py
+++ Lesson_3/task1.py
@@ -11,9 +11,6 @@ def operationMultiplication (NumberFirst, NumberSecond):
 def operationDivision (NumberFirst, NumberSecond):
     return NumberFirst / NumberSecond
 
-def operationSquareNum(NumberFirst):
-    return NumberFirst * NumberFirst
-
 def main():
     print("You can do operation: Addition(+), Subtraction(-), Multiplication(*), Division(/)")
     operation = input("Choise the operation:")
@@ -22,15 +19,16 @@ def main():
     choice_operation(NumberFirst,NumberSecond,operation) 
 
 def choice_operation(NumberFirst,NumberSecond,operation):
+    print("Result operation: ", end = "")
     match operation:
         case "+":
-            print("Result operation: " + str(operationAddition(NumberFirst, NumberSecond)))
+            print(operationAddition(NumberFirst, NumberSecond))
         case "-":
-            print("Result operation: " + str(operationSubtraction(NumberFirst, NumberSecond)))
+            print(operationSubtraction(NumberFirst, NumberSecond))
         case "*":
-            print("Result operation: " + str(operationMultiplication(NumberFirst, NumberSecond)))
+            print(operationMultiplication(NumberFirst, NumberSecond))
         case "/":
-            print("Result operation: " + str(operationDivision(NumberFirst, NumberSecond))) 
+            print(operationDivision(NumberFirst, NumberSecond))
         case _:
             print("Іnvalid value. Try again...")
             main()

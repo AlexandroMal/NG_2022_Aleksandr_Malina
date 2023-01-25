474685da65b1a6567fc994836f928f0ae65456e2
Alexandr Malina <Sashamalina8695@gmail.com>
2023-01-25 18:27:26 +0200

    made a count using 'count'

--- Lesson_2/task1.py
+++ Lesson_2/task1.py
@@ -1,16 +1,21 @@
+import math
+
 string = input("Enter a string: ")
 letters = {}
+
 
 for elements in string.lower():
     if elements != " ":
         try:
-            letters[elements] = letters[elements] + 1
+            letters[elements] = string.count(elements)
         except:
             letters[elements] = 1
+
 
 print("\nSorted by letter: ")
 for keys, values in sorted(letters.items()):
     print((f"{keys}: {values}"))
+
 
 print("\nSorted by number: ")
 for keys, values in sorted(letters.items(), key=lambda value: value[1], reverse=True):
